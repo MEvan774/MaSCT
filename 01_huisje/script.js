0027e89e1440eda4e6f4e2e5e98c792e790211d3
Henk466 <55583139+Henk466@users.noreply.github.com>
2020-01-15 19:50:34 +0100

    Commit 2 met 3 opdrachten af

--- 01_huisje/script.js
+++ 01_huisje/script.js
@@ -15,47 +15,49 @@ canvas.height = height;
 
 // Lijn Dak Voorkant
 
-context.beginPath()
-//context.fillStyle = “grey”;
-context.moveTo(200,300);
-context.lineTo(600,400);
-context.lineTo(440,120);
+context.beginPath();
+context.fillStyle = "grey";
+context.moveTo(900, 200);
+context.lineTo(1000, 300);
+context.lineTo(800, 400);
 context.closePath();
 context.stroke();
-//context.fill()
+context.fill()
 
 
 //Lijn Dak Zijkant
 
-context.beginPath()
-//context.fillStyle = “red”;
-context.moveTo(440,120);
-context.lineTo(1140,80);
-context.lineTo(1280,360);
-context.lineTo(600,400);
+context.beginPath();
+context.fillStyle = "red";
+context.moveTo(500, 100);
+context.lineTo(900, 200);
+context.lineTo(800, 400);
+context.lineTo(400, 300);
 context.closePath();
 context.stroke();
-//context.fill()
+context.fill()
+
+
+//Lijn Muur Voorkant
+
+context.beginPath();
+context.fillStyle = "grey";
+context.moveTo(1000, 300);
+context.lineTo(1000, 500);
+context.lineTo(800, 600);
+context.lineTo(800, 400);
+context.closePath();
+context.stroke();
+context.fill()
 
 //Lijn Muren Zijkant
 
-context.beginPath()
-//context.fillStyle = “grey”;
-context.lineTo(1260,360);
-context.lineTo(600,400);
-context.lineTo(600,700);
-context.lineTo(1260,660);
+context.beginPath();
+context.fillStyle = "grey";
+context.moveTo(800, 600);
+context.lineTo(400, 500);
+context.lineTo(400, 300);
+context.lineTo(800, 400);
 context.closePath();
 context.stroke();
-//context.fill()
-
-//Lijn Muren Voorkant
-context.beginPath()
-//context.fillStyle = “grey”;
-context.lineTo(600,400);
-context.lineTo(600,700);
-context.lineTo(220,605);
-context.lineTo(220,305);
-context.closePath();
-context.stroke();
-//context.fill()
+context.fill()
--- 04_Array/script.js
+++ 04_Array/script.js
@@ -9,6 +9,7 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
+let circles = [];
 
 // hier kun je op de canvas tekenen
 
@@ -23,21 +24,32 @@ canvas.height = height;
 
 
 let circleObject = {};
-circleObject.x = 60;
-circleObject.y = 100;
-circleObject.radius = 40;
-circleObject.color = "yellow";
-circleObject.speedX = 6;
-circleObject.speedY = 6;
+circleObject.x = getRandomInt(0,width);
+circleObject.y = getRandomInt(0,height);
+circleObject.radius = getRandomInt(5,20);
+circleObject.sAngle = 0;
+circleObject.eAngle = Math.PI *2;
+circleObject.color = colors[getRandomInt(0,colors.lenght)];
+circleObject.velocity_x = getRandomInt(2,5);
+circleObject.velocity_y = getRandomInt(2,5);
+
+//get Random
+function getRandomInt(min, max)
+{
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 
 circleObject.draw = function()
 {
 	context.beginPath();
-	context.fillStyle = "yellow"
 	context.arc(circleObject.x,circleObject.y,circleObject.radius,0,2*Math.PI);
+	context.closePath();
 	context.stroke();
+	context.fillStyle = this.color;
 	context.fill();
 }
 
@@ -47,6 +59,7 @@ circleObject.update = function()
 {
 	circleObject.x = circleObject.x + circleObject.speedX;
 	circleObject.y = circleObject.y + circleObject.speedY;
+	
 	if(circleObject.y > height - circleObject.radius || circleObject.y < circleObject.radius){
     circleObject.speedY = - circleObject.speedY ;
 	}
